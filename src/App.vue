c559f35286351294f1f9d7f777450a55ebbc2aeb
Linus Styrén <linus.styren@ga.ntig.se>
2021-05-03 17:21:52 +0200

    add PeopleTable.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PeopleTable />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PeopleTable from './components/PeopleTable.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PeopleTable
   }
 }
 </script>
---
+++ src/components/PeopleTable.vue
@@ -0,0 +1,24 @@
+!<template>
+  <table>
+  </table>
+</template>
+
+
+<script>
+import db from '@/db'
+
+export default {
+  data() {
+    return {
+      people: []
+    }
+  },
+  firebase: {
+    people: db.ref('people')
+  }
+}
+</script>
+
+<style>
+
+</style>
