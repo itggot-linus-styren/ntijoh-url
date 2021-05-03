b3c515baca0b8166c22a48f4946b9e9180118d2f
Linus Styrén <linus.styren@ga.ntig.se>
2021-05-03 17:41:29 +0200

    add purecss

--- src/components/PeopleTable.vue
+++ src/components/PeopleTable.vue
@@ -1,23 +1,25 @@
 <template>
   <div>
     <h1>People</h1>
-    <table>
-      <thead>
-        <th>firstname</th>
-        <th>lastname</th>
-        <th>age</th>
-        <th>email</th>
-      </thead>
-      <tbody>
-        <tr>
-          <td><input type="text" placeholder="firstname"></td>
-          <td><input type="text" placeholder="lastname"></td>
-          <td><input type="number" placeholder="age"></td>
-          <td><input type="email" placeholder="email"></td>
-        </tr>
-      </tbody>
-    </table>
-    <button>add person</button>
+    <form class="pure-form">
+      <table class="pure-table">
+        <thead>
+          <th>firstname</th>
+          <th>lastname</th>
+          <th>age</th>
+          <th>email</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td><input type="text" placeholder="firstname"></td>
+            <td><input type="text" placeholder="lastname"></td>
+            <td><input type="number" placeholder="age"></td>
+            <td><input type="email" placeholder="email"></td>
+          </tr>
+        </tbody>
+      </table>
+    </form>
+    <button class="pure-button">add person</button>
   </div>
 </template>
 
@@ -38,5 +40,7 @@ export default {
 </script>
 
 <style>
-
+table {
+  margin: 16px auto;
+}
 </style>
