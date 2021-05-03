f78908a452316ac2bbd56b9ef4e5b318aef0fe2f
Linus Styrén <linus.styren@ga.ntig.se>
2021-05-03 17:35:38 +0200

    create table with header

--- src/components/PeopleTable.vue
+++ src/components/PeopleTable.vue
@@ -1,6 +1,24 @@
-!<template>
-  <table>
-  </table>
+<template>
+  <div>
+    <h1>People</h1>
+    <table>
+      <thead>
+        <th>firstname</th>
+        <th>lastname</th>
+        <th>age</th>
+        <th>email</th>
+      </thead>
+      <tbody>
+        <tr>
+          <td><input type="text" placeholder="firstname"></td>
+          <td><input type="text" placeholder="lastname"></td>
+          <td><input type="number" placeholder="age"></td>
+          <td><input type="email" placeholder="email"></td>
+        </tr>
+      </tbody>
+    </table>
+    <button>add person</button>
+  </div>
 </template>
 
 
